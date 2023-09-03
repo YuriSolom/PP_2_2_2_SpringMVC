d18621f3e1c43c4c0f9ab0ee9a3f1f303959c318
YuriSolom <ziemssenjoach@gmail.com>
2023-09-04 00:53:22 +0300

    changed DI

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -10,12 +10,15 @@ import web.service.CarServiceImpl;
 
 @Controller
 public class CarsController {
-    @Autowired
-    CarService cars;
+    private final CarService cars;
+
+    public CarsController(CarService cars) {
+        this.cars = cars;
+    }
 
     @RequestMapping(value = "/cars")
     public String showCars(@RequestParam(value = "count", defaultValue = "-1") int a, Model model) {
-        cars = new CarServiceImpl();
+//        cars = new CarServiceImpl();
         model.addAttribute("cars", cars.getCars(a));
            return "cars";
     }
